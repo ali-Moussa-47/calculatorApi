836d1d6fc9526825af2a6de64874b5499e164b84
moussa <ali.moussa.1@ulaval.ca>
2025-04-07 10:26:30 -0400

    feat/ ajout du sentry

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 def create_app():
+    sentry_sdk.init(
+        dsn="https://7d24ad2245b497ba37bd4d65142cd3fc@o4508976581312512.ingest.us.sentry.io/4509112215601152",
+
+        integrations=[FlaskIntegration()],
+        traces_sample_rate=1.0,
+        environment="dev"
+    )
+
     app = Flask(__name__)
 
     from .routes import calc_bp
